deb44743266965e406bd7ef775135cc0ac08a482
Dema Adzhani <demaadzhani08@gmail.com>
2026-08-04 14:25:12 +0700

    bug

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,4 +1,5 @@
 export { useAuthStore } from './authStore';
 export { useWebsiteStore } from './websiteStore';
+export { useBrandStore } from './brandStore';
 export { useToastStore, toast } from './toastStore';
 export { useOnboardingStore } from './onboardingStore';
