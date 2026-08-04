9b92f825c9dbe2705f271dc181bdf93373dc4162
HadessCry <hazzelgo@gmail.com>
2026-08-04 11:47:24 +0700

    fix: make pages/index.js exports explicit to eliminate IDE linter warnings

--- resources/js/pages/index.js
+++ resources/js/pages/index.js
@@ -1,8 +1,25 @@
-// Role-based & feature modules
-export * from './admin';
-export * from './user';
-export * from './auth';
-export * from './builder';
+// Admin Pages
+export { default as AdminDashboard } from './admin/AdminDashboard';
+export { default as AdminUsers } from './admin/AdminUsers';
+export { default as AdminWebsites } from './admin/AdminWebsites';
+export { default as AdminTemplates } from './admin/AdminTemplates';
+export { default as AdminCategories } from './admin/AdminCategories';
+export { default as AdminAnalytics } from './admin/AdminAnalytics';
+export { default as AdminSettings } from './admin/AdminSettings';
 
-// Core switcher entry point
+// User Pages
+export { default as UserDashboard } from './user/UserDashboard';
+export { default as Websites } from './user/Websites';
+export { default as Templates } from './user/Templates';
+export { default as Profile } from './user/Profile';
+export { default as Settings } from './user/Settings';
+
+// Auth Pages
+export { default as Login } from './auth/Login';
+export { default as Register } from './auth/Register';
+
+// Builder Page
+export { default as Builder } from './builder/Builder';
+
+// Main Dashboard Switcher
 export { default as Dashboard } from './Dashboard';
